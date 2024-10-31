b15aa9434e24895c2fc9487f4f253f8ede239222
张亚飞 <672233580@qq.com>
2024-10-31 16:52:02 +0800

    进制转换

--- DoExercise0223/main.swift
+++ DoExercise0223/main.swift
@@ -911,13 +911,13 @@ import Foundation
  */
 
 
-let line = readLine()
-let regux = try NSRegularExpression(pattern: "[^a-zA-Z]")
-let res = regux.stringByReplacingMatches(in: line!, range: NSMakeRange(0, line!.count), withTemplate: " ")
-let array = res.split(separator: " ")
-for i in (0..<array.count).reversed() {
-    print(array[i], terminator: " ")
-}
+//let line = readLine()
+//let regux = try NSRegularExpression(pattern: "[^a-zA-Z]")
+//let res = regux.stringByReplacingMatches(in: line!, range: NSMakeRange(0, line!.count), withTemplate: " ")
+//let array = res.split(separator: " ")
+//for i in (0..<array.count).reversed() {
+//    print(array[i], terminator: " ")
+//}
 
 
 
@@ -935,3 +935,57 @@ for i in (0..<array.count).reversed() {
 //    }
 //    print(count)
 //}
+
+//描述
+//原理：ip地址的每段可以看成是一个0-255的整数，把每段拆分成一个二进制形式组合起来，然后把这个二进制数转变成
+//一个长整数。
+//举例：一个ip地址为10.0.3.193
+//每段数字             相对应的二进制数
+//10                   00001010
+//0                    00000000
+//3                    00000011
+//193                  11000001
+//
+//组合起来即为：00001010 00000000 00000011 11000001,转换为10进制数就是：167773121，即该IP地址转换后的数字就是它了。
+//
+//数据范围：保证输入的是合法的 IP 序列
+//
+//输入描述：
+//输入
+//1 输入IP地址
+//2 输入10进制型的IP地址
+//
+//输出描述：
+//输出
+//1 输出转换成10进制的IP地址
+//2 输出转换后的IP地址
+
+while let line = readLine() {
+    if line.contains("."){
+        let parts = line.split(separator: ".").compactMap{ UInt8($0)}
+        var binaryValue = ""
+        for part in parts {
+            //每段ip转换成二进制
+            let binaryPart = String(part, radix: 2)
+            //为了保持每个段8位对其 补齐零
+            binaryValue += String(repeating: "0", count: 8 - binaryPart.count) + binaryPart
+        }
+        
+//        print(binaryValue)
+        if let decimalValue = UInt32(binaryValue, radix: 2) {
+            print(decimalValue)
+        }
+    } else {
+        if let decimal  = UInt32(line) {
+//            print(decimal)
+            var ipParts = [String]()
+            for i in stride(from: 24, through: 0, by: -8) {
+                let byte = (decimal >> i) & 0xFF
+                ipParts.append(String(byte))
+//                print("\(i) ---- \(byte)")
+            }
+            print(ipParts.map{$0}.joined(separator: "."))
+        }
+            
+    }
+}
